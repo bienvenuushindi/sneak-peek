f15cfeb6ed5ddaaeca670d38ecfeee98f7a09783
usbbush <jeanbienvenusb@gmail.com>
2022-06-28 14:30:38 +0200

    Refactor to use JavaScript classes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,74 +1,97 @@
-let Books = [];
 const bookForm = document.getElementById('book-form');
 const bookList = document.querySelector('.books');
+const msgBlock = document.getElementById('msg-block');
 
-function Book(title, author) {
-  this.title = title;
-  this.author = author;
+class Book {
+  static list = [];
+  
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+  
+  store() {
+    Book.list.push(this);
+  }
+  
+  static remove(index) {
+    return Book.list.splice(index, 1);
+  }
+  
+  get details() {
+    return this.title + ' by ' + this.author;
+  }
+  
+  static updateLocalStorage() {
+    localStorage.setItem('books', JSON.stringify(Book.list));
+  }
+  
+  static retrieveFormLocalStorage() {
+    Book.list = [...JSON.parse(localStorage.getItem('books'))];
+    return Book.list
+  }
 }
 
-function createBook(form) {
-  const title = form.elements.title.value;
-  const author = form.elements.author.value;
-  return new Book(title, author);
-}
-
-function store(books) {
-  window.localStorage.setItem('books', JSON.stringify(books));
-}
-
-function addBook() {
-  Books.push(createBook(bookForm));
-  store(Books);
-}
-
-function setText(target, value) {
-  target.innerHTML = value;
-}
-
-function displayInfoMsg() {
-  const noBookFoundMsg = '<li class=\'list-group-item\'><div class=\'alert alert-info\'> No Book Found, Please Add First !!!</div></li>';
-  bookList.classList.remove('border');
-  setText(bookList, noBookFoundMsg);
-}
-
-function removeBook(index) {
-  Books.splice(index, 1);
-  store(Books);
-}
-
-function listBooks() {
-  const collection = JSON.parse(localStorage.getItem('books'));
-  return collection.map((item, index) => ` <li class='book-item d-flex justify-content-between p-2 ${index % 2 === 0 ? 'bg-secondary text-white' : ''}'>
+class BookUI {
+  static noBookFoundMsg = 'No Book Found, Please Add First !';
+  static deleteMsg = 'Book deleted successfully.';
+  static insertMsg = 'Book added successfully.';
+  
+  static set(target, value) {
+    target.innerHTML = value;
+  }
+  
+  static alert(msg = this.noBookFoundMsg, classList = 'alert-info') {
+    return `<div class=\'alert ${classList}\'>${msg}</div> `;
+  }
+  
+  static listAll() {
+    return Book.retrieveFormLocalStorage().map((book, index) => ` <li class='book-item d-flex justify-content-between p-2 ${index % 2 === 0 ? 'bg-secondary text-white' : ''}'>
                     <div class='book-info d-flex w-100 align-items-center text-20'>
-                    <div class='book-title mr-1'> ${item.title} </div><span>&nbsp; by &nbsp;</span>
-                    <div class='book-author mr-1'> ${item.author} </div>
-</div>
+                         <div class='book-title mr-1'> ${book.title} </div>
+                         <span>&nbsp; by &nbsp;</span>
+                         <div class='book-author mr-1'> ${book.author} </div>
+                    </div>
                     <div class='action '>
                         <button type='button' class='remove-button btn btn-danger ml-auto small' id='${index}'>Remove</button>
                     </div>
                     <hr>
                 </li> `).join(' ');
+  }
 }
 
 bookForm.addEventListener('submit', (ev) => {
-  addBook();
+  const title = bookForm.elements.title.value;
+  const author = bookForm.elements.author.value;
+  const book = new Book(title, author);
+  book.store();
+  Book.updateLocalStorage();
+  const itemList = BookUI.listAll;
+  BookUI.set(bookList, itemList());
+  BookUI.set(msgBlock, BookUI.alert(BookUI.insertMsg, 'alert-success'));
   bookList.classList.add('border');
-  setText(bookList, listBooks());
   ev.preventDefault();
 });
 
 bookList.addEventListener('click', (ev) => {
   if (ev.target.classList.contains('remove-button')) {
-    removeBook(ev.target.id);
-    setText(bookList, listBooks());
-    if (Books.length === 0) displayInfoMsg();
+    const id = ev.target.id;
+    Book.remove(id);
+    Book.updateLocalStorage();
+    const itemList = BookUI.listAll;
+    BookUI.set(bookList, itemList());
+    BookUI.set(msgBlock, BookUI.alert(BookUI.deleteMsg, 'alert-danger'));
+    if (Book.list.length === 0) {
+      BookUI.set(msgBlock, BookUI.alert());
+      bookList.classList.remove('border');
+    }
   }
 });
 
-if (!localStorage.getItem('books') || JSON.parse(localStorage.getItem('books')).length === 0) {
-  displayInfoMsg();
+if (!localStorage.getItem('books') || Book.retrieveFormLocalStorage().length === 0) {
+  BookUI.set(msgBlock, BookUI.alert());
+  bookList.classList.remove('border');
 } else {
-  Books = [...JSON.parse(localStorage.getItem('books'))];
-  setText(bookList, listBooks());
+  const itemList = BookUI.listAll;
+  BookUI.set(bookList, itemList());
 }
