587f6f113ad875338db4351a100fff10ef2014bf
usbbush <jeanbienvenusb@gmail.com>
2022-06-28 09:36:32 +0200

    Implement peer to peer code review recommendation
        add some margin-top for the h1.
        increase the font-size for the book title and author name.

--- assets/js/app.js
+++ assets/js/app.js
@@ -40,7 +40,7 @@ function removeBook(index) {
 function listBooks() {
   const collection = JSON.parse(localStorage.getItem('books'));
   return collection.map((item, index) => ` <li class='book-item d-flex justify-content-between p-2 ${index % 2 === 0 ? 'bg-secondary text-white' : ''}'>
-                    <div class='book-info d-flex w-100'>
+                    <div class='book-info d-flex w-100 align-items-center text-20'>
                     <div class='book-title mr-1'> ${item.title} </div><span>&nbsp; by &nbsp;</span>
                     <div class='book-author mr-1'> ${item.author} </div>
 </div>
