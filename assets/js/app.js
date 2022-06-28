a7dddec6c4b1f94117ce8048427a8b4a97d882c8
Muhmmd-Usama <mummdusama@gmail.com>
2022-06-28 17:13:16 +0200

    Fix some issus in js file and linters Errors

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,47 +4,49 @@ const msgBlock = document.getElementById('msg-block');
 
 class Book {
   static list = [];
-  
+
   constructor(title, author) {
     this.title = title;
     this.author = author;
   }
-  
+
   store() {
     Book.list.push(this);
   }
-  
+
   static remove(index) {
     return Book.list.splice(index, 1);
   }
-  
+
   get details() {
-    return this.title + ' by ' + this.author;
+    return ((this.title) + (this.author));
   }
-  
+
   static updateLocalStorage() {
     localStorage.setItem('books', JSON.stringify(Book.list));
   }
-  
+
   static retrieveFormLocalStorage() {
     Book.list = [...JSON.parse(localStorage.getItem('books'))];
-    return Book.list
+    return Book.list;
   }
 }
 
 class BookUI {
   static noBookFoundMsg = 'No Book Found, Please Add First !';
+
   static deleteMsg = 'Book deleted successfully.';
+
   static insertMsg = 'Book added successfully.';
-  
+
   static set(target, value) {
     target.innerHTML = value;
   }
-  
+
   static alert(msg = this.noBookFoundMsg, classList = 'alert-info') {
-    return `<div class=\'alert ${classList}\'>${msg}</div> `;
+    return `<div class='alert ${classList}'>${msg}</div> `;
   }
-  
+
   static listAll() {
     return Book.retrieveFormLocalStorage().map((book, index) => ` <li class='book-item d-flex justify-content-between p-2 ${index % 2 === 0 ? 'bg-secondary text-white' : ''}'>
                     <div class='book-info d-flex w-100 align-items-center text-20'>
