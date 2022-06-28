1f4bfd9f51b17ade21c58908e3acfb99a00ad072
usbbush <jeanbienvenusb@gmail.com>
2022-06-28 17:53:38 +0200

    Fix linters error

--- assets/js/app.js
+++ assets/js/app.js
@@ -61,12 +61,12 @@ class Book {
 }
 
 bookForm.addEventListener('submit', (ev) => {
-  let title = bookForm.elements.title.value;
-  let author = bookForm.elements.author.value;
+  const title = bookForm.elements.title.value;
+  const author = bookForm.elements.author.value;
   const book = new Book(title, author);
   book.store();
-  bookForm.elements.title.value='';
-  bookForm.elements.author.value='';
+  bookForm.elements.title.value = '';
+  bookForm.elements.author.value = '';
   Book.updateLocalStorage();
   const itemList = Book.listAll;
   Book.setUI(bookList, itemList());
