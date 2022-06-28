ef5ac8211a7a429f1d86c9b4ab8a9896dc4f8e09
usbbush <jeanbienvenusb@gmail.com>
2022-06-28 17:51:18 +0200

    Clear the inputs fileds after pressing the Add button

--- assets/js/app.js
+++ assets/js/app.js
@@ -61,10 +61,12 @@ class Book {
 }
 
 bookForm.addEventListener('submit', (ev) => {
-  const title = bookForm.elements.title.value;
-  const author = bookForm.elements.author.value;
+  let title = bookForm.elements.title.value;
+  let author = bookForm.elements.author.value;
   const book = new Book(title, author);
   book.store();
+  bookForm.elements.title.value='';
+  bookForm.elements.author.value='';
   Book.updateLocalStorage();
   const itemList = Book.listAll;
   Book.setUI(bookList, itemList());
